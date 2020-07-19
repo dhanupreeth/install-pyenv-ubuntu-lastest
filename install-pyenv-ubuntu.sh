d4407394d9f4580b378400a3fd4152adfa890d9f
Dhanesh Kumar <dhanupreeth@gmail.com>
2020-07-19 20:06:06 +0530

    first commit

---
+++ install-pyenv-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install git python3-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl
+sudo pip3 install virtualenvwrapper
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'pyenv virtualenvwrapper' >> ~/.bashrc
+
+exec $SHELL
+
